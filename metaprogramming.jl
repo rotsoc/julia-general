88fa1af983f5da9e4cb5e8a051e7459eed2c2358
rotsoc <socrtjin@gmail.com>
2018-11-16 17:41:10 +0200

    metaprogramming

--- metaprogramming.jl
+++ metaprogramming.jl
@@ -1,1 +1,19 @@
+using MacroTools
+
 :a, :b, :c, :foo, :α, :call # These are all symbols
+:(a+1),:(sin(b)),:(c(10)),:(foo(a,b,c)),:(foo(α^2+1)) # These are all expressions
+
+ex = :(x+3)
+
+ex.args[2] = :z
+ex.args[3] = 4
+
+ex.args[1] = :*
+
+Meta.show_sexpr(ex)
+
+ex2 = :(x + 3y)
+
+Meta.show_sexpr(ex2)
+
+Meta.parse("x+3y")
---
+++ modexp.jl
@@ -0,0 +1,14 @@
+function modexp(x,y,N)
+    # Input: wo n-bit integers x and N, an integer exponent y
+    # Output: xʸ mod N
+
+    if y==0
+        return 1
+    end
+    z = modexp(x,floor(y/2),N)
+    if y%2 == 0
+        return mod(z^2,N)
+    else
+        return mod(x*z^2,N)
+    end
+end
